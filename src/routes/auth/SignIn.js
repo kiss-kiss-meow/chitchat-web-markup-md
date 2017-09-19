6efe61baf3dd0e0571623204096aa11063314aa8
Igor Moiseyenko <imoiseyenko93@gmail.com>
2017-09-19 18:36:34 +0300

    SignIn route.

---
+++ src/routes/auth/SignIn.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { withStyles } from 'material-ui/styles'
+import Grid from 'material-ui/Grid'
+import SignIn from 'components/SignIn'
+
+const styles = theme => ({
+  root: {
+    paddingTop: theme.spacing.unit * 8
+  }
+})
+
+const SignInRoute = ({ classes }) => (
+  <div className={classes.root}>
+    <Grid container justify="center">
+      <Grid item md={3} sm={6} xs={10}>
+        <SignIn />
+      </Grid>
+    </Grid>
+  </div>
+)
+
+export default withStyles(styles)(SignInRoute)
