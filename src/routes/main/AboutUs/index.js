63880e9a6c4aafe1262257e49ce91270200c2eaa
Igor Moiseyenko <imoiseyenko93@gmail.com>
2017-09-20 04:17:59 +0300

    AboutUs route stylings.

--- src/routes/main/AboutUs/index.js
+++ src/routes/main/AboutUs/index.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import Grid from 'material-ui/Grid'
+import { withStyles } from 'material-ui/styles'
 import AboutUs from 'components/AboutUs'
+import styles from './styles'
 
-const AboutUsRoute = () => (
-  <Grid container>
+const AboutUsRoute = ({ classes }) => (
+  <Grid container className={classes.root} spacing={0}>
     <Grid item xs={12}>
       <AboutUs />
     </Grid>
   </Grid>
 )
 
-export default AboutUsRoute
+export default withStyles(styles)(AboutUsRoute)
