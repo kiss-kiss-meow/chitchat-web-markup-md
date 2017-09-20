7224e6f3b6c087dbb2f2c0f2b349d2e5767a6270
Igor Moiseyenko <imoiseyenko93@gmail.com>
2017-09-20 04:02:39 +0300

    Main route stylings.

--- src/routes/main/index.js
+++ src/routes/main/index.js
@@ -1,20 +1,28 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import Grid from 'material-ui/Grid'
+import { withStyles } from 'material-ui/styles'
 import Home from 'routes/main/Home'
 import AboutUsRoute from 'routes/main/AboutUs'
 import Header from 'components/Header'
 import Footer from 'components/Footer'
+import styles from './styles'
 
-const MainRoute = () => (
-  <Grid container>
+const MainRoute = ({ classes }) => (
+  <Grid
+    container
+    className={classes.root}
+    direction="column"
+    wrap="nowrap"
+    spacing={0}
+  >
     {/* Header */}
-    <Grid item xs={12}>
+    <Grid item>
       <Header />
     </Grid>
 
     {/* Routes */}
-    <Grid item xs={12}>
+    <Grid item className={classes.content}>
       <Switch>
         <Route path="/about" component={AboutUsRoute} />
         <Route path="/" component={Home} />
@@ -22,10 +30,10 @@ const MainRoute = () => (
     </Grid>
 
     {/* Footer */}
-    <Grid item xs={12}>
+    <Grid item>
       <Footer />
     </Grid>
   </Grid>
 )
 
-export default MainRoute
+export default withStyles(styles)(MainRoute)
---
+++ src/routes/main/styles.js
@@ -0,0 +1,8 @@
+export default () => ({
+  root: {
+    height: '100%'
+  },
+  content: {
+    flex: 1
+  }
+})
