8d85fc9691f42cbc5dc669b8af0557600e8719ad
Igor Moiseyenko <imoiseyenko93@gmail.com>
2017-09-23 03:19:08 +0300

    Message History Item component.

---
+++ src/components/MessageHistoryItem/index.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { GridListTile } from 'material-ui/GridList'
+import { withStyles } from 'material-ui/styles'
+import Typography from 'material-ui/Typography'
+import styles from './styles'
+
+const genericClassNames = (base, additional) => (classes, message) => {
+  let classNames = [classes[base]]
+  if (message.isMe) classNames.push(classes[additional])
+  return classNames.join(' ')
+}
+
+const rootClassNames = genericClassNames('root', 'myMessageAlign')
+const messageClassNames = genericClassNames('message', 'myMessageBackground')
+
+const MessageHistoryItem = ({ message, classes }) => (
+  <GridListTile
+    key={message.id}
+    cols={1}
+    className={rootClassNames(classes, message)}
+  >
+    <Typography type="body1" className={messageClassNames(classes, message)}>
+      {message.text}
+    </Typography>
+  </GridListTile>
+)
+
+export default withStyles(styles)(MessageHistoryItem)
---
+++ src/components/MessageHistoryItem/styles.js
@@ -0,0 +1,21 @@
+export default ({ mainColorPalette }) => ({
+  root: {
+    // Material UI workaround: GridListTile isn't received 'width', 'height', 'padding' after decomposition (GridList & GridListTile are in different files)
+    width: '100%',
+    height: 'auto',
+    padding: '6px'
+    // Material UI workaround end.
+  },
+  message: {
+    display: 'inline-flex',
+    padding: '5px 10px',
+    backgroundColor: mainColorPalette[50],
+    borderRadius: '10px'
+  },
+  myMessageAlign: {
+    textAlign: 'right'
+  },
+  myMessageBackground: {
+    backgroundColor: mainColorPalette[200]
+  }
+})
